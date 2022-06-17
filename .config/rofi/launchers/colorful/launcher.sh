ac854b2c68f1251129f2e2bbc57806840df0e624
Yurimaster2112 <Yurimaster2112@gmail.com>
2022-06-16 22:05:57 -0400

    change rofi settings

--- .config/rofi/launchers/colorful/launcher.sh
+++ .config/rofi/launchers/colorful/launcher.sh
@@ -11,11 +11,11 @@
 # style_1     style_2     style_3     style_4     style_5     style_6
 # style_7     style_8     style_9     style_10    style_11    style_12
 
-theme="style_1"
+theme="style_7"
 dir="$HOME/.config/rofi/launchers/colorful"
 
 # dark
-ALPHA="#00000000"
+ALPHA="#233231"
 BG="#000000ff"
 FG="#FFFFFFff"
 SELECT="#101010ff"
@@ -45,7 +45,7 @@ cat > $dir/colors.rasi <<- EOF
 EOF
 
 # comment these lines to disable random style
-themes=($(ls -p --hide="launcher.sh" --hide="colors.rasi" $dir))
-theme="${themes[$(( $RANDOM % 12 ))]}"
+#themes=($(ls -p --hide="launcher.sh" --hide="colors.rasi" $dir))
+#theme="${themes[$(( $RANDOM % 12 ))]}"
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
