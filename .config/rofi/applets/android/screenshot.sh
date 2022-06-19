c6b4468dc1efc6a0eafa46cc302435e642f91b1c
Yurimaster2112 <Yurimaster2112@gmail.com>
2022-06-19 19:40:44 -0400

    add save to clipboard feature

--- .config/rofi/applets/android/screenshot.sh
+++ .config/rofi/applets/android/screenshot.sh
@@ -18,21 +18,24 @@ chosen="$(echo -e "$options" | $rofi_command -p '' -dmenu -selected-row 0)"
 case $chosen in
     $screen)
 		if [[ -f /usr/bin/scrot ]]; then
-			sleep 1; scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+			#sleep 1; scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+            sleep 1; scrot '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -targe image/png -i $f'; 
 		else
 			msg
 		fi
         ;;
     $area)
 		if [[ -f /usr/bin/scrot ]]; then
-			scrot -s 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+			#scrot -s 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+            scrot -s '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -targe image/png -i $f';
 		else
 			msg
 		fi
         ;;
     $window)
 		if [[ -f /usr/bin/scrot ]]; then
-			sleep 1; scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+			#sleep 1; scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+            sleep 1; scrot -u '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -targe image/png -i $f'; 
 		else
 			msg
 		fi
